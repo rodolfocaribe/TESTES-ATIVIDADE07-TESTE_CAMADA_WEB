ae449f6573e815785b234ed51d6256ab5f561052
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2023-03-16 20:24:25 -0300

    Criou testes para somar subtrair e multiplicar. Correção de código.

--- calculadora-maven-aula/src/main/java/calculadora/Calculadora.java
+++ calculadora-maven-aula/src/main/java/calculadora/Calculadora.java
@@ -6,11 +6,11 @@ public class Calculadora {
 	}
 
 	public int subtrair(int num1, int num2) {
-		return num1 + num2;
+		return num1 - num2;
 	}
 
 	public int multiplicar(int num1, int num2) {
-		return num1 + num2;
+		return num1 * num2;
 	}
 
 	public int dividir(int num1, int num2) {
--- calculadora-maven-aula/src/test/java/calculadora/CalculadoraTest.java
+++ calculadora-maven-aula/src/test/java/calculadora/CalculadoraTest.java
@@ -2,22 +2,54 @@ package calculadora;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-class CalculadoraTest {
+
+import org.junit.jupiter.api.Assertions;
+public class CalculadoraTest {
 
     @Test
-    void somar() {
+    public void testarSomaDoisInteirosSucesso() {
+    	//Arrange - Definir o cenário de teste
+    	Calculadora calculadora = new Calculadora();
+    	int entradaNumeroUm = 10;
+    	int entradaNumeroDois = 5;
+    	int saidaEsperada = 15;
+    	
+    	//Act - Executaros métodos que estão sendo testados
+    	int saidaObtida = calculadora.somar(entradaNumeroUm, entradaNumeroDois);
+    	
+    	//Assign - Validar o resultado obtido
+    	Assertions.assertEquals(saidaEsperada, saidaObtida);
     }
-
+    
     @Test
-    void subtrair() {
-    }
-
+    public void testarSubtrairDoisInteirosSucesso() {
+    	//Arrange - Definir o cenário de teste
+    	Calculadora calculadora = new Calculadora();
+    	int entradaNumeroUm = 10;
+    	int entradaNumeroDois = 5;
+    	int saidaEsperada = 5;
+    	
+    	//Act - Executaros métodos que estão sendo testados
+    	int saidaObtida = calculadora.subtrair(entradaNumeroUm, entradaNumeroDois);
+    	
+    	//Assign - Validar o resultado obtido
+    	Assertions.assertEquals(saidaEsperada, saidaObtida);
+    }    
+    
     @Test
-    void multiplicar() {
-    }
+    public void testarMultiplicarDoisInteirosSucesso() {
+    	//Arrange - Definir o cenário de teste
+    	Calculadora calculadora = new Calculadora();
+    	int entradaNumeroUm = 10;
+    	int entradaNumeroDois = 5;
+    	int saidaEsperada = 50;
+    	
+    	//Act - Executaros métodos que estão sendo testados
+    	int saidaObtida = calculadora.multiplicar(entradaNumeroUm, entradaNumeroDois);
+    	
+    	//Assign - Validar o resultado obtido
+    	Assertions.assertEquals(saidaEsperada, saidaObtida);
+    }        
 
-    @Test
-    void dividir() {
-    }
+    
 }
