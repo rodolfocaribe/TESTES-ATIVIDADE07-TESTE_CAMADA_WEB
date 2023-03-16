4865499e8713ad830c6f1fb6ece12bc06ff120d8
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2023-03-16 19:17:29 -0300

    Criar projeto base da calculadora

---
+++ calculadora-maven-aula/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,21 @@
+package calculadora;
+
+public class Calculadora {
+	public int somar(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public int subtrair(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public int multiplicar(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	public int dividir(int num1, int num2) {
+		if (num2 == 0)
+			return -1;
+		return num1 / num2;
+	}
+}
