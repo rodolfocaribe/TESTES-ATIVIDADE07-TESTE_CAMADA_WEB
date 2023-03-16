5a8369d9b34a79200cbcf0705e7de21d43815e00
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2023-03-16 19:35:12 -0300

    Criar a classe de Teste CalculadoraTest

---
+++ calculadora-maven-aula/src/test/java/calculadora/CalculadoraTest.java
@@ -0,0 +1,23 @@
+package calculadora;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CalculadoraTest {
+
+    @Test
+    void somar() {
+    }
+
+    @Test
+    void subtrair() {
+    }
+
+    @Test
+    void multiplicar() {
+    }
+
+    @Test
+    void dividir() {
+    }
+}
