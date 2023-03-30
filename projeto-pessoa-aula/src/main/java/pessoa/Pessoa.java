10d01c8df3dd13579c8f0c0142ed0de4a8815b7c
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2023-03-30 20:41:05 -0300

    TDD - etapa 4 - Refatorar - Fim versão 1

--- projeto-pessoa-aula/src/main/java/pessoa/Pessoa.java
+++ projeto-pessoa-aula/src/main/java/pessoa/Pessoa.java
@@ -1,15 +1,17 @@
 package pessoa;
 
 public class Pessoa {
-private String nome;
-	
+	private String nome;
+
 	public void setNome(String nome) {
-		if (nome.length()<2 || nome.length()>100)
+		if (nome.length() >= 2 && nome.length() <= 100)
+			this.nome = nome.toUpperCase();
+		else
 			throw new IllegalArgumentException("O nome precisa ter entre 2 e 100 caracteres");
-		this.nome = nome.toUpperCase();		
+
 	}
 
-	public String getNome() {		
+	public String getNome() {
 		return this.nome;
 	}
 
