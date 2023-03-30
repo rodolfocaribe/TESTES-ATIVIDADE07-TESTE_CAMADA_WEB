27cffbda093dd71574e32f32c8ed506dd86dce4f
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2023-03-30 20:31:35 -0300

    TDD - Etapa 3 - codificar classe Pessoa

--- projeto-pessoa-aula/src/main/java/pessoa/Pessoa.java
+++ projeto-pessoa-aula/src/main/java/pessoa/Pessoa.java
@@ -1,6 +1,16 @@
 package pessoa;
 
 public class Pessoa {
+private String nome;
 	
+	public void setNome(String nome) {
+		if (nome.length()<2 || nome.length()>100)
+			throw new IllegalArgumentException("O nome precisa ter entre 2 e 100 caracteres");
+		this.nome = nome.toUpperCase();		
+	}
+
+	public String getNome() {		
+		return this.nome;
+	}
 
 }
