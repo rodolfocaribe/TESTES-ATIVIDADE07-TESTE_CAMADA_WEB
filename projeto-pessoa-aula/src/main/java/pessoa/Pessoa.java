4fbf4a414611f20570b1fd3dcbad3df5e528db42
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2023-03-30 20:24:17 -0300

    Atualizar classe Pessoa

--- projeto-pessoa-aula/src/main/java/pessoa/Pessoa.java
+++ projeto-pessoa-aula/src/main/java/pessoa/Pessoa.java
@@ -1,14 +1,6 @@
 package pessoa;
 
 public class Pessoa {
-	private String nome;
 	
-	public void setNome(String nome) {
-		this.nome = nome;		
-	}
-
-	public String getNome() {		
-		return this.nome;
-	}
 
 }
