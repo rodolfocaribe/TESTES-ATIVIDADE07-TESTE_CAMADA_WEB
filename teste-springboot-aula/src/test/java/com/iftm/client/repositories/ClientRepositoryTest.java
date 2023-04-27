ee89d4c0f7274a5cb00f21e4e172ef3a83984eb3
Bruno Queiroz <bruno.queiroz@iftm.edu.br>
2023-04-27 19:13:44 -0300

    projetos atualizados

--- teste-springboot-aula/src/test/java/com/iftm/client/repositories/ClientRepositoryTest.java
+++ teste-springboot-aula/src/test/java/com/iftm/client/repositories/ClientRepositoryTest.java
@@ -16,28 +16,6 @@ public class ClientRepositoryTest {
     @Autowired
     private ClientRepository repositorio;
 
-    @Test
-    @DisplayName("Testa se o método findById realmente retorna o cliente correto.")
-    void testaSeBuscaObjetoQuandoExisteId() {
-        long idExistente = 1;
-        String nomeEsperado = "Conceição Evaristo";
-        String cpfEsperado = "10619244881";
-        Optional<Client> resultado = repositorio.findById(idExistente);
-        assertTrue(resultado.isPresent(), "Objeto não encontrado.");
-        assertEquals(nomeEsperado, resultado.get().getName());
-        assertEquals(cpfEsperado, resultado.get().getCpf());
-    }
 
-    @Test
-    @DisplayName("Testa se o método deleteById realmente apaga o cliente correto.")
-    void testaSeApagarObjetoQuandoExisteId() {
-        long idExistente =1L;
-        int numElementosEsperados = 11;
-        repositorio.deleteById(idExistente);
-        Optional<Client> resultado = repositorio.findById(idExistente);
-        assertFalse(resultado.isPresent(), "Objeto encontrado.");
-        assertEquals(numElementosEsperados, repositorio.count(),
-                "Numero de elementos do Banco de dados incorretos");
-    }
 
 }
